db9d711c499d654ffaf47795936686d39c72e44d
Nishchal Singi <71981858+Nishchal-007@users.noreply.github.com>
2021-09-08 17:02:51 +0530

    Boilerplate Code

--- server.py
+++ server.py
@@ -29,3 +29,33 @@ def Signup_View():
 
 if __name__ == "__main__":
     app.run(port=9001)
+  
+"""
+STORED PROCEDURE FOR USERS SIGNUP
+
+DELIMITER $$
+CREATE PROCEDURE `sp_userCreate`(
+        IN p_username VARCHAR(20),
+        IN p_email TEXT,
+        IN p_age BIGINT,
+        IN p_password TEXT
+    )
+BEGIN
+        if ( select exists (select 1 from auths_table where username = p_username) ) THEN
+            select 'Username Exists !!';
+        ELSE
+            insert into users (
+                username,
+                email,
+                age,
+                user_password
+            )
+            values (
+                p_username,
+                p_email,
+                p_age,
+                p_password
+            );
+        END IF;
+END$$
+"""
